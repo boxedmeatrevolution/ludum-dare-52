5550e3812429bbf7ccea0d208d84615329abe6dd
Duane Byer <duane.byer@gmail.com>
2023-01-08 10:20:26 -0500

    Sliding

--- entities/player.gd
+++ entities/player.gd
@@ -12,7 +12,10 @@ const BLOCK_MASK := 0b0000000000000010
 
 const DASH_SPEED := 600.0
 const SLIDE_SPEED_MIN := 200.0
+const SLIDE_SPEED_FALL_OFF := 200.0
+const SLIDE_SMOOTH_CROSS := 0.5
 const SLIDE_FRICTION := 400.0
+const DASH_MIN_UPWARDNESS := -0.8
 
 onready var sprite := $Sprite
 onready var animation_player := $AnimationPlayer
@@ -24,7 +27,6 @@ var stand_segment_idx := 0
 var stand_position := 0.0
 var slide_velocity := 0.0
 var dash_velocity := Vector2.ZERO
-var target_direction := Vector2.ZERO
 var rotation_offset := 30.0
 var head_collision_offset := 60.0
 
@@ -37,20 +39,21 @@ func _physics_process(delta: float) -> void:
 		var normal := get_normal()
 		sprite.rotation = normal.angle() + 0.5 * PI
 		if Input.is_action_just_released("dash"):
-			target_direction = (get_global_mouse_position() - position - rotation_offset_vector()).normalized()
-			if target_direction.dot(normal) > -0.8:
+			var target_direction = (get_global_mouse_position() - position - rotation_offset_vector()).normalized()
+			if target_direction.dot(normal) > DASH_MIN_UPWARDNESS:
 				state = State.DASH
 				animation_player.play("dash")
 				dash_velocity = DASH_SPEED * target_direction
 				position += dash_velocity * delta + 1.0 * normal
 				rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
 	elif state == State.SLIDE:
-		sprite.rotation = get_normal().angle() + 0.5 * PI
+		var normal := get_normal()
+		sprite.rotation = normal.angle() + 0.5 * PI
 		if slide_velocity == 0.0:
 			state = State.STAND
 		else:
 			var max_distance := abs(slide_velocity * delta)
-			while max_distance > 0.0:
+			while max_distance > 0.0 && state == State.SLIDE:
 				var segment : SegmentShape2D = stand_block.segments[stand_segment_idx]
 				var segment_length := (segment.b - segment.a).length()
 				var edge_distance : float
@@ -69,37 +72,85 @@ func _physics_process(delta: float) -> void:
 				var raycast := space_state.intersect_ray(position, next_position, [stand_block], BLOCK_MASK)
 				if raycast:
 					var old_tangent := get_tangent()
-					stand_block = raycast.collider
-					stand_segment_idx = raycast.shape
-					stand_position = convert_pos_global_to_seg(stand_block, stand_segment_idx, raycast.position)
-					slide_velocity *= get_tangent().dot(old_tangent)
-					next_position = convert_pos_seg_to_global(stand_block, stand_segment_idx, stand_position)
-					max_distance -= (next_position - position).length()
-					position = next_position
+					var old_normal := get_normal()
+					var new_stand_block : Block = raycast.collider
+					var new_stand_segment_idx : int = raycast.shape
+					var new_stand_segment : SegmentShape2D = new_stand_block.segments[new_stand_segment_idx]
+					var new_tangent := (new_stand_segment.b - new_stand_segment.a).normalized()
+					var tangent_cross := old_tangent.cross(new_tangent)
+					if abs(tangent_cross) < SLIDE_SMOOTH_CROSS:
+						stand_block = new_stand_block
+						stand_segment_idx = new_stand_segment_idx
+						stand_position = convert_pos_global_to_seg(stand_block, stand_segment_idx, raycast.position)
+						#slide_velocity *= get_tangent().dot(old_tangent)
+						next_position = convert_pos_seg_to_global(stand_block, stand_segment_idx, stand_position)
+						max_distance -= (next_position - position).length()
+						position = next_position
+					elif sign(slide_velocity) * tangent_cross < 0.0 || abs(slide_velocity) < SLIDE_SPEED_FALL_OFF:
+						slide_velocity = 0.0
+						max_distance = 0.0
+					else:
+						state = State.DASH
+						animation_player.play("dash")
+						dash_velocity = slide_velocity * old_tangent
+						position += dash_velocity * delta + 1.0 * old_normal
+						rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
 				elif edge_distance < max_distance:
 					var old_tangent := get_tangent()
+					var old_normal := get_normal()
+					var new_stand_segment_idx : int
+					var new_stand_segment : SegmentShape2D
+					var new_stand_position : float
 					if slide_velocity > 0.0:
-						stand_segment_idx = (stand_segment_idx + 1) % stand_block.segments.size()
-						stand_position = 0.0
+						new_stand_segment_idx = (stand_segment_idx + 1) % stand_block.segments.size()
+						new_stand_segment = stand_block.segments[new_stand_segment_idx]
+						new_stand_position = 0.0
 					else:
-						stand_segment_idx = (stand_segment_idx + stand_block.segments.size() - 1) % stand_block.segments.size()
-						var prev_segment : SegmentShape2D = stand_block.segments[stand_segment_idx]
-						var prev_segment_length := (prev_segment.b - prev_segment.a).length()
-						stand_position = prev_segment_length
-					slide_velocity *= get_tangent().dot(old_tangent)
-					max_distance -= edge_distance
-					position = convert_pos_seg_to_global(stand_block, stand_segment_idx, stand_position)
+						new_stand_segment_idx = (stand_segment_idx + stand_block.segments.size() - 1) % stand_block.segments.size()
+						new_stand_segment = stand_block.segments[new_stand_segment_idx]
+						var new_segment_length := (new_stand_segment.b - new_stand_segment.a).length()
+						new_stand_position = new_segment_length
+					var new_tangent := (new_stand_segment.b - new_stand_segment.a).normalized()
+					var tangent_cross := old_tangent.cross(new_tangent)
+					if abs(tangent_cross) < SLIDE_SMOOTH_CROSS:
+						stand_segment_idx = new_stand_segment_idx
+						stand_position = new_stand_position
+						#slide_velocity *= get_tangent().dot(old_tangent)
+						max_distance -= edge_distance
+						position = convert_pos_seg_to_global(stand_block, stand_segment_idx, stand_position)
+					elif sign(slide_velocity) * tangent_cross < 0.0 || abs(slide_velocity) < SLIDE_SPEED_FALL_OFF:
+						if slide_velocity < 0.0:
+							stand_position = 0.0
+						else:
+							stand_position = segment_length
+						position = convert_pos_seg_to_global(stand_block, stand_segment_idx, stand_position)
+						slide_velocity = 0.0
+						max_distance = 0.0
+					else:
+						state = State.DASH
+						animation_player.play("dash")
+						dash_velocity = slide_velocity * old_tangent
+						position += dash_velocity * delta + 1.0 * old_normal
+						rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
 				else:
 					stand_position = next_stand_position
 					position = next_position
 					max_distance = 0.0
+		if Input.is_action_just_released("dash"):
+			var target_direction = (get_global_mouse_position() - position - rotation_offset_vector()).normalized()
+			if target_direction.dot(normal) > DASH_MIN_UPWARDNESS:
+				state = State.DASH
+				animation_player.play("dash")
+				dash_velocity = DASH_SPEED * target_direction
+				position += dash_velocity * delta + 1.0 * normal
+				rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
 	elif state == State.DASH:
-		if dash_velocity.angle() + 0.5 * PI != sprite.rotation:
-			rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
+		#if dash_velocity.angle() + 0.5 * PI != sprite.rotation:
+		#	rotate_around_rotation_offset(dash_velocity.angle() + 0.5 * PI)
 		var space_state := get_world_2d().direct_space_state
 		var delta_position := dash_velocity * delta
 		var next_position := position + delta_position
-		var raycast := space_state.intersect_ray(position, next_position + target_direction * head_collision_offset, [], BLOCK_MASK)
+		var raycast := space_state.intersect_ray(position, next_position + dash_velocity.normalized() * head_collision_offset, [], BLOCK_MASK)
 		if raycast:
 			state = State.SLIDE
 			animation_player.play("slide")
