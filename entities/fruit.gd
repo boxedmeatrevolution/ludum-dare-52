b9a83695dea5b38208d524db22243693ef7b529e
Duane Byer <duane.byer@gmail.com>
2023-01-09 11:47:35 -0500

    Wiggles

--- entities/fruit.gd
+++ entities/fruit.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+onready var sprite := $Sprite
+var time := 0.0
 
 func _ready() -> void:
 	$"/root/GameController".num_fruits_remaining += 1
 
 
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	time += delta
+	sprite.position.y = 5.0 * sin(2 * PI * time / 1.5 + 2 * PI * position.x / 180.0)
 
 func harvest() -> void:
 	$"/root/GameController".num_fruits_remaining -= 1
